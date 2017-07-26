5ed247736990b1deb4cde08f88d0a46d566269b4
Rathi <github email address>
2017-07-26 13:48:51 -0400

    included youtube js

---
+++ src/Youtube.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+var Youtube   = React.createClass({
+  render () {
+    return (
+    <div>
+      <div className = "youtube">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/c9V3FBJ8FoA"
+        frameBorder="0"
+        allowFullScreen>
+        </iframe>
+
+      </div>
+      <button>Get youtube video</button>
+    </div>
+    )
+  }
+})
+
+export default Youtube
