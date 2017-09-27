16ca6d8ea650dd292a6ab3a3d538db0746a9cc3b
Rathi <github email address>
2017-09-27 12:04:36 -0400

    added missig file

--- src/Youtube.js
+++ src/Youtube.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import config from './config.js'
+import config from './config.js';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
